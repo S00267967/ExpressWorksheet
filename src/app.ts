bf70c245f185dfff9e67e7547f41653f7c66459d
CP <S00267967@atu.ie>
2026-09-24 17:11:13 +0100

    Last minute push they kicking us out

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,20 @@
-import express, {Application, Request, Response} from "express" ; 
+import express, {Application, request, Request, Response} from "express" ; 
 import carRoutes from './routes/cars';
 import { env } from "./config/env";
 import { connectDB } from "./config/database";
+import {authenticateKey} from './middleware/auth.middleware';
+
 
 const PORT = env.port; 
 const app: Application = express(); 
 
-app.use('/api/v1/cars', carRoutes);
 app.use(express.json());
+    console.log('Request body:', request.body);
+app.use('/api/v1/cars', authenticateKey, carRoutes);
 app.use((req, _res, next) => { 
     console.log(`${req.method} ${req.originalUrl}`); 
     next(); 
 }); 
-app.get("/ping", async (_req : Request, res: Response) => { 
-    res.json({ 
-        message: "Hello, Message! I am the god of the application." }); }); 
-        app.listen(PORT, () => { 
-app.get('/templeOS', async (_req : Request, res: Response) => { 
-    res.json({ 
-        message: "I am Rasta the Pasta", 
-    }); 
-
-}); 
-
 const startServer = async () => {
   await connectDB();
 
@@ -33,5 +25,19 @@ const startServer = async () => {
 };
 
 startServer(); 
+/***
+ * app.use(express.json());
+    console.log('Request body:', request.body);
 
-})
+app.get("/ping", async (_req : Request, res: Response) => { 
+    res.json({ 
+        message: "Hello, Message! I am the god of the application." }); }); 
+        app.listen(PORT, () => { 
+app.get('/templeOS', async (_req : Request, res: Response) => { 
+    res.json({ 
+        message: "I am Rasta the Pasta", 
+    }); 
+
+}); 
+ 
+ */
